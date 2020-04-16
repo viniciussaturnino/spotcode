3a0cf886a275484ab40c002ee8f50d39bc733488
viniciussaturnino07 <viniciussaturnino07@hotmail.com>
2020-04-16 20:29:23 -0300

    added bulma random elements

---
+++ app/javascript/app.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import 'react-bulma-components/dist/react-bulma-components.min.css'
+import { Button } from 'react-bulma-components'
+import 'app.scss'
+
+const App = () => {
+    return(
+        <Fragment>
+            <Button color='primary'>Teste</Button>
+        </Fragment>
+    )
+}
+
+export default App
