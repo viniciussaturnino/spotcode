6abe4b2a962a7635a326d61119fe314b6796afda
viniciussaturnino07 <viniciussaturnino07@hotmail.com>
2020-04-20 20:02:08 -0300

    show the real songs in your respective album

--- app/javascript/components/musics/index.js
+++ app/javascript/components/musics/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Columns, Button } from 'react-bulma-components'
 import Music from './music'
@@ -8,6 +8,15 @@ const PlaySequenceButton = styled(Button)`
 `
 
 const Musics = (props) => {
+    const [songs, setSongs] = useState([])
+    const [playing, setPlaying] = useState([])
+
+    useEffect(() => {
+        setSongs(props.songs.map((song, key) =>
+            <Music song={song} playing={playing.id == song.id} setPlaying={setPlaying} key={key} artist_name={props.artist_name}/>
+        ))
+    }, [props.songs, playing])
+
     return(
         <Fragment>
             <Columns className='is-mobile is-centered'>
@@ -15,9 +24,7 @@ const Musics = (props) => {
                     <PlaySequenceButton className='is-medium' color='primary' outlined>Tocar Aleatoriamente</PlaySequenceButton>
                 </Columns.Column>
             </Columns>
-            <Music/>
-            <Music/>
-            <Music/>
+            {songs}
         </Fragment>
     )
 }
