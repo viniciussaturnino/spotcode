a7e4d661559fc8e65d570529ff4b0259ca6a3cc6
viniciussaturnino07 <viniciussaturnino07@hotmail.com>
2020-04-20 23:29:30 -0300

    now songs is playing. Discovery a error with song random button position wich i resolve latter

--- app/javascript/components/musics/index.js
+++ app/javascript/components/musics/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react'
+import React, { Fragment, useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import { Columns, Button } from 'react-bulma-components'
 import Music from './music'
@@ -10,6 +10,17 @@ const PlaySequenceButton = styled(Button)`
 const Musics = (props) => {
     const [songs, setSongs] = useState([])
     const [playing, setPlaying] = useState([])
+    const AudioRef = useRef()
+
+    useEffect(() =>{
+        if(AudioRef.current != null){
+            AudioRef.current.pause()
+            AudioRef.current.load()
+            if(playing.id){
+                AudioRef.current.play()
+            }
+        }
+    },[playing])
 
     useEffect(() => {
         setSongs(props.songs.map((song, key) =>
@@ -21,7 +32,12 @@ const Musics = (props) => {
         <Fragment>
             <Columns className='is-mobile is-centered'>
                 <Columns.Column desktop={{size: 2}} mobile={{size: 12}} className='has-text-centered'>
-                    <PlaySequenceButton className='is-medium' color='primary' outlined>Tocar Aleatoriamente</PlaySequenceButton>
+                    <PlaySequenceButton className='is-medium' color='primary' outlined>
+                            Tocar Aleatoriamente
+                    </PlaySequenceButton>
+                    <audio controls ref={AudioRef}>
+                        <source src={playing.file_url}/>
+                    </audio>
                 </Columns.Column>
             </Columns>
             {songs}
